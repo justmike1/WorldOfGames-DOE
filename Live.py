9243efa98a7a32f9fea6a517abed3b48948d4f58
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-10-25 18:15:29 +0300

    Update Live.py

--- Live.py
+++ Live.py
@@ -1,15 +1,15 @@
 import logging
 
-def MemoryGame(difficulty):
+def MemoryGame(difficulty):     #Starts the game's script at requested difficulty
     print("Starting Memory Game with difficulty", difficulty)
 
-def GuessGame(difficulty):
+def GuessGame(difficulty):      #Starts the game's script at requested difficulty
     print("Starting Guess Game with difficulty", difficulty)
 
-def CurrencyRouletteGame(difficulty):
+def CurrencyRouletteGame(difficulty):       #Starts the game's script at requested difficulty
     print("Starting Currency Roulette Game with difficulty", difficulty)
 
-def start_game(game_id, difficulty):
+def start_game(game_id, difficulty):        #game_id equals d['game'], vice versa about difficulty, look main() function
     if game_id == 1:
         MemoryGame(difficulty)
     elif game_id == 2:
@@ -18,9 +18,10 @@ def start_game(game_id, difficulty):
         CurrencyRouletteGame(difficulty)
 
 def load_game():
-    try:
+    try:        # Has to start with an try exception to get through the ValueError (typing a letter string)
         name = input("Hello There! What is your name? ")
         print(f"Hello {name} and welcome to the World of Games (WoG). \nHere you can find many cool games to play!")
+        # The variable that saves the Game & Difficulty inputs (has to be the first in the block & empty dict)
         d = {}
         d['Game'] = int(input("Please choose a game to play: \n 1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back. \n 2. Guess Game - guess a number and see if you chose like the computer. \n 3. Currency Roulette - try and guess the value of a random amount of USD to ILS. \n : "))
         if d['Game'] not in range(1, 4):
@@ -30,14 +31,14 @@ def load_game():
         if d['Difficulty'] not in range(1, 6):
             print('num is invalid')
             return (main())
-    except ValueError:
+    except ValueError:  #    If the user inputs e.g 'a' or another letter, it restarts (thanks for the headache Dan)
         print('num is invalid')
         return(main())
-
     return (name, d)
 
-def main():
+def main():     #The main function which starts the whole process, after it starts load_game for user's inputs, it starts the chosen game at certain difficulty
     name, d = load_game()
     start_game(d["Game"], d["Difficulty"])
 
-main()
+if __name__ == "__main__":
+    main()
