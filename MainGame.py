e3a486c9cd2591d58e875eeccf1dd735d883bcac
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-10-26 22:15:10 +0300

    Update MainGame.py

--- MainGame.py
+++ MainGame.py
@@ -1,6 +1,5 @@
 from Live import main
 import logging
-from abc import ABC, abstractmethod
 
 FORMAT = '%(message)s'
 logging.basicConfig(format=FORMAT)
@@ -13,19 +12,5 @@ while True:
         logging.warning('\ninput is invalid, try again: ')
     else:
         main()
+        break
 
-class WoG(ABC):
-    @abstractmethod
-    pass
-
-#game_1 = MemoryGame
-class game_1(WoG):
-    pass
-    
-#game_2 = GuessGame
-class game_2(WoG):
-    pass
-
-#game_3 = CurrencyRouletteGame
-class game_3(WoG):
-    pass
