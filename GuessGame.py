b527549b108b25c16d171d0244268c7b3a6b45da
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-10-27 15:51:39 +0300

    Update GuessGame.py

--- GuessGame.py
+++ GuessGame.py
@@ -6,17 +6,17 @@ class Game2:
         self.play()
 
     def generate_number(self):
-        self.secret_number = random.randint(1, self.Difficulty) 
+        self.secret_number = random.randint(1, self.Difficulty)
 
     def get_guess_from_user(self):
-        self.User_Guess = int(input(f'Guess a number between 1 to {self.Difficulty}: '))
+        self.User_Guess = input(f'Guess a number between 1 to {self.Difficulty}: ')
 
     def compare_results(self):
         i = 1
         UserWins = False
         while i < 3:        # the user gets 3 tries
-            if self.User_Guess == self.secret_number:
-                print('Correct, You made it!')
+            if int(self.User_Guess) == self.secret_number:
+                print(f'Congrats, You made it! Secret number was {self.secret_number}!')
                 UserWins = True
                 break
             else:
