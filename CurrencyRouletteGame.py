f77439dc293305bc72fe8005a90b860c9276db7f
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-10-27 17:58:56 +0300

    Update and rename World of Games.py to CurrencyRouletteGame.py

---
+++ CurrencyRouletteGame.py
@@ -0,0 +1,41 @@
+import random
+import requests
+import json
+
+class Game3:
+    def __init__(self, difficulty):
+        url = "https://free.currconv.com/api/v7/convert?q=USD_ILS&compact=ultra&apiKey=3eee6e211600d6203628"
+        response = requests.get(url)
+        data = response.text
+        parsed = json.loads(data)
+        self.USD_ILS = float(parsed["USD_ILS"])
+        self.Difficulty = difficulty
+        self.play()
+
+    def get_money_interval(self):
+        self.random_number = random.randint(1, 100)
+        for self.Difficulty in self.USD_ILS
+
+    def get_guess_from_user(self):
+        self.User_Guess = input(f'Guess a the value of USD\ILS ')
+        '''
+        i = 1
+        UserWins = False
+        while i < 3:  # the user gets 3 tries
+            if self.User_Guess == str(self.secret_number):
+                print(f'Congrats, You made it! Secret number was {self.secret_number}!')
+                UserWins = True
+                break
+            else:
+                print('The number you guessed is incorrect')
+                i += 1
+                self.get_guess_from_user()
+        '''  
+        return UserWins
+
+    def play(self):
+        self.get_money_interval()
+        return self.get_guess_from_user()
+
+
+
--- World of Games.py
+++
@@ -1,1 +0,0 @@
-https://free.currconv.com/api/v7/convert?q=USD_ILS&compact=ultra&apiKey=3eee6e211600d6203628
