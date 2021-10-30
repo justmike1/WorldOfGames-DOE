3666154e75622623e92de5749e782888bb674ade
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-10-30 17:04:58 +0300

    Update MemoryGame.py

--- MemoryGame.py
+++ MemoryGame.py
@@ -7,39 +7,27 @@ class Game1:
         self.play()
 
     def generate_sequence(self):
-        res = random.sample(range(1, 101), self.Difficulty)
-        res_str = f"Random number list is {res}"
+        self.res = random.sample(range(1, 101), self.Difficulty)
+        res_str = f"Random number list is {self.res}"
         print(res_str, flush=True, end='\r')
         time.sleep(3)
-        print('    '*len(res_str))
-
-
-
+        print('    ' * len(res_str))
 
     def get_list_from_user(self):
-        self.user_res = random.sample(range(int(input(''))))
+        self.user_res = random.sample(range(int(input('Guess the generated list: '))))
 
+    def compare_results(self):
+        i = 1
+        while i < 3:  # the user gets 3 tries
+            if self.user_res not in self.res:
+                print('The number you guessed is incorrect')
+                i += 1
+                self.get_list_from_user()
+            else:
+                print(f'Congrats, You made it! Generated list was {self.res}!')
+                break
 
     def play(self):
         self.generate_sequence()
         self.get_list_from_user()
-       # return self.is_list_equal
-
-'''
-    def is_list_equal(self):
-        i = 1
-        UserWins = False
-        while i < 3:
-            if self.User_Guess == self.secret_number:
-                print('Correct, You made it!')
-                UserWins = True
-                break
-            else:
-                print('The number you guessed is incorrect')
-                i += 1
-                self.get_guess_from_user()
-        return UserWins
-'''
-
-
-
+        return self.compare_results()
