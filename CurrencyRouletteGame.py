ed39d21e1753fcca11c8b81c2d8d50d803826e43
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-11-01 19:35:53 +0200

    Update CurrencyRouletteGame.py

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -9,15 +9,18 @@ class Game3:
 
     def generate_number(self):
         url = "https://free.currconv.com/api/v7/convert?q=USD_ILS&compact=ultra&apiKey=3eee6e211600d6203628" #  the free API
-        response = requests.get(url)
-        data = response.text
-        parsed = json.loads(data)
-        self.USD_ILS = float(parsed["USD_ILS"])     #get the value
-        self.random_number = random.randint(1, 101)     #generate random number
-        print(f'{self.random_number} USD')
-        self.gen_number = round(self.random_number//self.USD_ILS)     # round it to integer
-        self.gen_list = list(range(self.gen_number-self.Difficulty, self.gen_number+self.Difficulty)) #generate a list in the range of difficulty
-        #print(f'{self.gen_list}') #Delete the sharp symbol when you want to test
+        res = requests.get(url)
+        if res.status_code != 200:
+            return -1
+        else:
+            data = res.text
+            parsed = json.loads(data)
+            self.USD_ILS = float(parsed["USD_ILS"])     #get the value
+            self.random_number = random.randint(1, 101)     #generate random number
+            print(f'{self.random_number} USD')
+            self.gen_number = round(self.random_number//self.USD_ILS)     # round it to integer
+            self.gen_list = list(range(self.gen_number-self.Difficulty, self.gen_number+self.Difficulty)) #generate a list in the range of difficulty
+            #print(f'{self.gen_list}') #Delete the sharp symbol when you want to test
 
     def get_guess_from_user(self):
         self.User_Guess = int(input(f'Guess the number in ILS: '))
