11f4b6e69beff71311c7628cd3c92c244a7365ce
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-11-02 17:39:08 +0200

    Update MainGame.py

--- MainGame.py
+++ MainGame.py
@@ -1,15 +1,11 @@
-from Live import main
-import logging
+from abc import ABC, abstractmethod
 
-FORMAT = '%(message)s'
-logging.basicConfig(format=FORMAT)
+class WoG(ABC):
+    def __init__(self, difficulty):
+        self.Difficulty = difficulty
+        self.play()
 
-while True:
-    exit = input('Are you sure you want to play? (y/n): ')
-    if exit.lower() == 'n':
-        break
-    elif exit.lower() != 'y':
-        logging.warning('\ninput is invalid, try again: ')
-    else:
-        main()
-        break
+    def play(self):
+        self.generate()
+        self.get_input_from_user()
+        return self.compare_results()
