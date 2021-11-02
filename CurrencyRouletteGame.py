6a2deef06def7c797670acc7509309663cae9e98
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-11-02 17:36:15 +0200

    Update CurrencyRouletteGame.py

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -1,13 +1,10 @@
+from MainGame import *
 import random
 import requests
 import json
 
-class Game3:
-    def __init__(self, difficulty):
-        self.Difficulty = difficulty
-        self.play()
-
-    def generate_number(self):
+class Game3(WoG):
+    def generate(self):
         url = "https://free.currconv.com/api/v7/convert?q=USD_ILS&compact=ultra&apiKey=3eee6e211600d6203628" #  the free API
         res = requests.get(url)
         if res.status_code != 200:
@@ -22,7 +19,7 @@ class Game3:
             self.gen_list = list(range(self.gen_number-self.Difficulty, self.gen_number+self.Difficulty)) #generate a list in the range of difficulty
             #print(f'{self.gen_list}') #Delete the sharp symbol when you want to test
 
-    def get_guess_from_user(self):
+    def get_input_from_user(self):
         self.User_Guess = int(input(f'Guess the number in ILS: '))
 
     def compare_results(self):
@@ -31,15 +28,10 @@ class Game3:
             if self.User_Guess not in self.gen_list:
                 print('The number you guessed is incorrect')
                 i += 1
-                self.get_guess_from_user()
+                self.get_input_from_user()
             else:
                 print(f'Congrats, You made it! Generated number was {self.gen_number}!')
                 break
-
-    def play(self):
-        self.generate_number()
-        self.get_guess_from_user()
-        return self.compare_results()
 
 
 
