dabcf1731d07fd1ae20d0af03a58fdd6001cf060
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-11-02 17:34:53 +0200

    Update MemoryGame.py

--- MemoryGame.py
+++ MemoryGame.py
@@ -1,24 +1,21 @@
+from MainGame import *
 import random
 import time
 
-class Game1:
-    def __init__(self, difficulty):
-        self.Difficulty = difficulty
-        self.play()
-
-    def generate_sequence(self):
+class Game1(WoG):
+    def generate(self):
         self.res = random.sample(range(1, 101), self.Difficulty)
         res_str = f"Random number list is {self.res}"
         print(res_str, flush=True, end='\r')
         time.sleep(3)
         print('    ' * len(res_str))
 
-    def get_list_from_user(self):
+    def get_input_from_user(self):
         try:    # must be a try with except because list doesn't support isdigit...
             self.user_res = list(map(int, input('Guess the generated list: ').split()))
         except ValueError:
             print('input is invalid')
-            return self.get_list_from_user()
+            return self.get_input_from_user()
 
 
     def compare_results(self):
@@ -27,12 +24,8 @@ class Game1:
             if self.user_res != self.res:
                 print('The number you guessed is incorrect')
                 i += 1
-                self.get_list_from_user()
+                self.get_input_from_user()
             else:
                 print(f'Congrats, You made it! Generated list was {self.res}!')
                 break
 
-    def play(self):
-        self.generate_sequence()
-        self.get_list_from_user()
-        return self.compare_results()
