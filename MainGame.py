d07f03598242356633a5e7b7641251c7e2e5590c
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-11-18 14:19:58 +0200

    Update MainGame.py

--- MainGame.py
+++ MainGame.py
@@ -1,6 +1,16 @@
-from abc import ABC, abstractmethod
+from abc import ABC
+import logging
+
 
 class WoG(ABC):
+    FORMAT = '%(message)s'
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%m-%d %H:%M')
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+
     def __init__(self, difficulty):
         self.Difficulty = difficulty
         self.play()
@@ -9,3 +19,10 @@ class WoG(ABC):
         self.generate()
         self.get_input_from_user()
         return self.compare_results()
+
+class Scores:
+    def add_score(self):
+        self.scores_file = open("Scores.txt", "a")
+        self.points_of_winning = (self.Difficulty*3)+5
+        self.scores_file.write(f"\n{str(self.points_of_winning)}")
+        self.scores_file.close()
